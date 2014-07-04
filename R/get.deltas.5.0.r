d8c3d8b1a08bf74c49a3fc258aadb5f2f06349b3
eldarrak <eldarrak@gmail.com>
2014-07-04 18:03:53 +0200

    sc

--- R/get.deltas.5.0.r
+++ R/get.deltas.5.0.r
@@ -33,9 +33,13 @@ if (return.all.out) {return(all.out)
 } else {return(Res)}
 }
 
-get.deltas.intermediate<-function(deltalim=c(-0.2, 0.2), start=c(0,0), Sigma=0.5, interval=600, short.run=T, Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=3, random.delta=T) {
+get.deltas.intermediate<-function(deltalim=c(-0.2, 0.2), start=c(0,0), Sigma=0.5, interval=600, short.run=T, Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=3, random.delta=T, fast=F) {
 	if (random.delta) {
-	Deltas<-rep(runif(ceiling((deltalim[2]-deltalim[1])/0.02), deltalim[1], deltalim[2]), repeats)
+		if (fast) {
+			Deltas<-rep(5, deltalim[1], deltalim[2]), repeats)
+		} else {
+			Deltas<-rep(runif(ceiling((deltalim[2]-deltalim[1])/0.02), deltalim[1], deltalim[2]), repeats)
+		}
 	} else {
 	Deltas<-rep(seq(deltalim[1], deltalim[2], 0.01), repeats)
 	}
@@ -48,12 +52,12 @@ return(Res)
 }
 
 # and now the next on that will iterate Sigma
-get.deltas.main<-function(deltalim=c(-0.2, 0.2), start=c(0,0), Sigmas=seq(0, 0.8, 0.1), interval=600, short.run=T, LogSlope=c(0.68, 0.4), Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=3, random.delta=T) {
+get.deltas.main<-function(deltalim=c(-0.2, 0.2), start=c(0,0), Sigmas=seq(0, 0.8, 0.1), interval=600, short.run=T, LogSlope=c(0.68, 0.4), Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=3, random.delta=T, fast=F) {
 
 Res<-c()
 for (i in Sigmas) {
 cat(Sigmas, "\n")
-Res<-rbind(Res,cbind(get.deltas.intermediate(deltalim=deltalim, start=start, Sigma=i, interval=interval, short.run=short.run, Parameters=Parameters, repeats=repeats, random.delta=random.delta), i))
+Res<-rbind(Res,cbind(get.deltas.intermediate(deltalim=deltalim, start=start, Sigma=i, interval=interval, short.run=short.run, Parameters=Parameters, repeats=repeats, random.delta=random.delta, fast=fast), i))
 print(Res)
 save(Res, file=paste("Res", start[2],"tmp.RData", sep="."))
 }
@@ -61,7 +65,7 @@ return(Res)
 }
 
 
-get.deltas.parallel<-function(deltalim=c(-0.2, 0.2), limits=c(-65,65), points=20, Sigmas=seq(0, 0.8, 0.1), interval=600, short.run=T, LogSlope=c(0.68, 0.4), threads=2, log.irrad.borders=c(-50, 50), Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=1, random.delta=T, wd="D://Geologgers") {
+get.deltas.parallel<-function(deltalim=c(-0.2, 0.2), limits=c(-65,65), points=20, Sigmas=seq(0, 0.8, 0.1), interval=600, short.run=T, LogSlope=c(0.68, 0.4), threads=2, log.irrad.borders=c(-50, 50), Parameters=list(Intercept=c(3.71, 1.25), LogSlope=c(0.72, 0.4)), repeats=1, random.delta=T, wd="D://Geologgers", fast=F) {
 
 # points means number of latitudes that should be used for the run..
 require(parallel)
@@ -82,7 +86,7 @@ Coords<-cbind(0, Lats)
     #tmp<-parallel:::clusterEvalQ(mycl, source(file.path(wd, "LightR_development_code\\get.slopes.5.0.r")))
     #tmp<-parallel:::clusterEvalQ(mycl, source(file.path(wd, "Geologgers\\LightR_development_code\\get.deltas.5.0.r")))
 	#Coords<-as.data.frame(Coords)
-	Res<-parApply(mycl, Coords, 1, FUN=function(x) as.data.frame(get.deltas.main(start=x,  deltalim=deltalim, Sigmas=Sigmas, interval=interval, short.run=short.run, LogSlope=LogSlope, Parameters=Parameters, repeats=1, random.delta=random.delta)))
+	Res<-parApply(mycl, Coords, 1, FUN=function(x) as.data.frame(get.deltas.main(start=x,  deltalim=deltalim, Sigmas=Sigmas, interval=interval, short.run=short.run, LogSlope=LogSlope, Parameters=Parameters, repeats=1, random.delta=random.delta, fast=fast)))
 	#Res1<-apply(Coords, 1, FUN=function(x) as.data.frame(get.deltas.main(start=x,  deltalim=deltalim, Sigmas=Sigmas, interval=interval, short.run=short.run, LogSlope=LogSlope, Parameters=Parameters, repeats=1, random.delta=random.delta)))
 	stopCluster(cl = mycl)
 	Res<-do.call(rbind.data.frame, Res)
