85037e1d09c7137e87428928d9e84b1e6b48691a
eldarrak <eldarrak@gmail.com>
2014-07-04 14:58:38 +0200

    whatsnew_change

--- R/whatsnew.R
+++ R/whatsnew.R
@@ -2,3 +2,4 @@
 # ver 0.0.2. May 15 2013 added median in the plotting function
 # ver 0.1.3 June 7 get.irradiance added
 # ver 0.1.4 June 25 correct.hours corrected for summer time.., also added some stability to the movies
+# towards_0.3 ver 0.1.5 - functions 5.0 added
