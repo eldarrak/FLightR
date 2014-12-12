24473f14707344e7e8f5b37f4094a1e1cac17f05
Eldar Rakhimberdiev <eldarrak@gmail.com>
2014-12-12 14:36:48 +0100

    em_pf_3.7 instead of many old functions

--- R/node.run.R
+++
@@ -1,4 +0,0 @@
-node.run<-function(x=x) {
-		Points.Land<-get("Points.Land", globalenv())
-		return(sun.matrix.internal(x, Points.Land))
-		}
--- R/pf.par.internal.R
+++
@@ -1,7 +0,0 @@
-
-pf.par.internal<-function(x, Current.Proposal) {
-  # this simple function is needed to save I/0 during parallel run. Being executed at a slave it creates inoput for the next function taking Current proposal from the slave and Parameters from the master
-  new.Parameters<-c(x=list(x), get("Parameters"), Current.Proposal=list(Current.Proposal))
-  Res<-do.call(generate.points.dirs, new.Parameters)
-  return(Res)
-}
