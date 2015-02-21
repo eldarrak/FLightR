bebd4332a6d076347b9dd527540fc1f0777f6c1c
Eldar Rakhimberdiev <eldarrak@gmail.com>
2015-02-21 10:43:15 +0100

    new lat calibration functions

--- R/brute.force.calibration.R
+++ R/brute.force.calibration.R
@@ -270,3 +270,224 @@ if (mode=="smart") {
 # ok this is it so far
 return(RES)
 }
+
+#==================================
+# here are the new functions for the latitude correction estimation...
+# they are not wrapped yet though..
+
+
+simulate.and.prepare.track<-function(measurement.period=60, saving.period=600, Parameters=Parameters, short.run=T, min.max.values=c(0, 64),log.light.borders=log(c(2,64)),Lat, first.date="2010-01-01 00:00:00") {
+
+# for this we will have to create a To.run.object...
+To.run<-data.frame(Slope.ideal=Parameters$LogSlope_1_minute[1], SD.ideal=Parameters$LogSlope_1_minute[2], Latitude=Lat) #
+
+Track<-simulate.track(measurement.period=measurement.period, saving.period=saving.period, To.run=To.run, Parameters=Parameters, min.max.values=min.max.values, short.run=short.run, Time.seq=NULL, Time.seq.saving=NULL, Lon=0, first.date=first.date)
+# ok, now we need to process it..
+# to prepare for the run..
+
+#==================================
+# saving and reading track file
+cat("   saving file\n")
+lig.data<-cbind(format(as.POSIXct(Track$gmt, tz="gmt",origin="1970-01-01"), format="%d/%m/%Y"), format(as.POSIXct(Track$gmt, tz="gmt",origin="1970-01-01"), format="%H:%M:%S"), round(exp(Track$LogLight)))
+
+File.name<-tempfile(pattern = "sim.no.move.", tmpdir = getwd(), fileext = ".csv")
+write.table(lig.data, file=File.name, sep = ",", dec = ".", qmethod="double", quote = FALSE,row.names = FALSE, col.names=FALSE)
+
+## no I wnat to work with the simulated data.
+
+require(FLightR)
+require(GeoLight)
+require(maptools)
+#data(wrld_simpl)
+require(fields)
+cat("   reading file\n")
+# set wd
+Data<-geologger.read.data(file=File.name)
+try(unlink(File.name))
+
+#========================================================
+#========================================================
+
+# new trick - let's try to load the real track
+cat("   GeoLight step\n")
+
+tw <- twilightCalc(Track$gmt, Track$light, allTwilights=T, ask=F, LightThreshold=log.light.borders[1]+1)
+GLtab   <- tw[[2]] # Table to proceed with GeoLight
+cat("automatically detected twilight:\n")
+print(table(GLtab[,3]))
+
+# as far as we have done an automatic detection we should check on whether the is no problems..
+# the idea is that each dusk should be follwoed by dawn...
+Index<-1:length(GLtab[,3])
+GLtab[Index[Index%%2==1],3]<-round(mean(GLtab[Index[Index%%2==1],3]))
+GLtab[Index[Index%%2==0],3]<-round(mean(GLtab[Index[Index%%2==0],3]))
+# 
+GLtab1<-GLtab
+Filtered_tw <- data.frame(datetime=as.POSIXct(c(GLtab1$tFirst,GLtab1$tSecond),"UTC"),type=c(GLtab1$type,ifelse(GLtab1$type==1,2,1)))
+Filtered_tw <- Filtered_tw[!duplicated(Filtered_tw$datetime),]
+Filtered_tw <- Filtered_tw[order(Filtered_tw[,1]),]
+
+# now I want to pair data and twilights..		  
+Filtered_tw$light<-approx(x=Track$gmt, y=Track$light, xout=Filtered_tw$datetime)$y
+Filtered_tw$id<-0
+Track$type.real<-Track$type
+Track$type<-0
+
+Track.new<-Track[names(Track) %in% c("gmt", "light", "type")]
+Track.new<-rbind(Track.new, data.frame(gmt= Filtered_tw$datetime, light=Filtered_tw$light, type=Filtered_tw$type))
+All.p<-Track.new[order(Track.new$gmt),]
+rownames(All.p)<-1:nrow(All.p)
+
+raw.Y.dusk<-correct.hours(Filtered_tw$datetime[Filtered_tw$type==2])
+raw.X.dusk<-as.numeric(Filtered_tw$datetime[Filtered_tw$type==2])
+Result.Dusk<-make.result.list(Data, raw.X.dusk, raw.Y.dusk)
+
+raw.Y.dawn<-correct.hours(Filtered_tw$datetime[Filtered_tw$type==1])
+raw.X.dawn<-as.numeric(Filtered_tw$datetime[Filtered_tw$type==1])
+Result.Dawn<-make.result.list(Data, raw.X.dawn, raw.Y.dawn)
+
+Result.all<-list(Final.dusk=Result.Dusk, Final.dawn=Result.Dawn)
+####
+
+##START POINTS###
+#all.out<-geologger.sampler.create.arrays(Index.tab, Points.Land, start=start)
+
+#================= END== =====================================================
+
+#=============================================================================
+
+Proc.data<-process.twilights(All.p, Filtered_tw, measurement.period=measurement.period, saving.period=saving.period)
+
+## Dusk
+Twilight.time.mat.dusk<-Proc.data$Twilight.time.mat.dusk
+Twilight.log.light.mat.dusk<-Proc.data$Twilight.log.light.mat.dusk
+
+## Dawn
+Twilight.time.mat.dawn<-Proc.data$Twilight.time.mat.dawn
+Twilight.log.light.mat.dawn<-Proc.data$Twilight.log.light.mat.dawn
+
+
+	Twilight.vector.dusk<-1:(dim(Twilight.time.mat.dusk)[2])
+ 	Twilight.vector.dawn<-1:(dim(Twilight.time.mat.dawn)[2])
+
+##########################
+# and now I want to save output..
+	Res<-list(Dusk=list(Twilight.vector=Twilight.vector.dusk, Twilight.log.light.mat=Twilight.log.light.mat.dusk, Twilight.time.mat=Twilight.time.mat.dusk), Dawn=list(Twilight.vector=Twilight.vector.dawn, Twilight.log.light.mat=Twilight.log.light.mat.dawn, Twilight.time.mat=Twilight.time.mat.dawn), Lat=Lat, Filtered_tw=Filtered_tw)
+	
+	return(Res)
+}
+
+
+get.grid.of.tracks<-function(measurement.period=60, saving.period=600, Parameters=Parameters, short.run=T, min.max.values=c(0, 64), log.light.borders=log(c(2,64)),   Lats, cluster=NULL, first.date="2010-01-01 00:00:00") {
+if (!is.null(cluster)) {
+	require(parallel)
+	#if (threads==-1) threads= detectCores()-1 # selecting all -1 available cores...
+	#cluster<-makeCluster(threads)
+		tmp<-parallel:::clusterSetRNGStream(cluster)
+		### we don' need to send all parameters to node. so keep it easy..
+		tmp<-parallel:::clusterExport(cluster, c( "simulate.and.prepare.track", "Parameters", "measurement.period","saving.period", "short.run", "min.max.values", "log.light.borders", "first.date", "simulate.track"),  envir=environment())
+		tmp<-parallel:::clusterEvalQ(cluster, library("FLightR"))
+		tmp<-parallel:::clusterEvalQ(cluster, library("GeoLight")) 
+		tmp<-parallel:::clusterEvalQ(cluster, library("maptools")) 
+		Tracks<-parLapply(cluster, Lats,fun=function(x) simulate.and.prepare.track(measurement.period=measurement.period, saving.period=saving.period,  Parameters=Parameters, min.max.values=min.max.values, short.run=short.run, log.light.borders=log.light.borders, Lat=x, first.date=first.date))
+	#stopCluster(mycl)
+} else {
+Tracks<-lapply(Lats,FUN=function(x) simulate.and.prepare.track(measurement.period=measurement.period, saving.period=saving.period,  Parameters=Parameters, min.max.values=min.max.values, short.run=short.run, log.light.borders=log.light.borders, Lat=x, first.date=first.date))
+}
+return(Tracks)
+}
+
+
+get.diff<-function(prepared.data, calibration, min.max.values=c(1, 1150), log.light.borders=log(c(2, 1100)), log.irrad.borders=c(-15, 50)) {
+
+	Points.Land<-as.matrix(expand.grid(0, seq(prepared.data$Lat-10, prepared.data$Lat+10, 0.5)))
+	Points.Land<-cbind(Points.Land, 1)
+
+	All.probs.dusk<-sapply(prepared.data$Dusk$Twilight.vector, FUN=get.prob.surface, Twilight.log.light.mat=prepared.data$Dusk$Twilight.log.light.mat, Twilight.time.mat=prepared.data$Dusk$Twilight.time.mat, dusk=T, Calib.param=calibration$Parameters$LogSlope, log.irrad.borders=log.irrad.borders, delta=prepared.data$delta, Points.Land=Points.Land,log.light.borders=log.light.borders, calibration=calibration)
+	
+	All.probs.dawn<-sapply(prepared.data$Dawn$Twilight.vector, FUN=get.prob.surface, Twilight.log.light.mat=prepared.data$Dawn$Twilight.log.light.mat, Twilight.time.mat=prepared.data$Dawn$Twilight.time.mat, dusk=F, Calib.param=calibration$Parameters$LogSlope, log.irrad.borders=log.irrad.borders, delta=prepared.data$delta, Points.Land=Points.Land,  log.light.borders=log.light.borders, calibration=calibration)
+
+	Phys.Mat<-c()
+	for (i in 1:nrow(prepared.data$Filtered_tw)) {
+	if (prepared.data$Filtered_tw$type[i]==2) {
+		Phys.Mat<-cbind(Phys.Mat, All.probs.dusk[,1])
+		All.probs.dusk<-as.matrix(All.probs.dusk[,-1])
+		} else {
+		Phys.Mat<-cbind(Phys.Mat, All.probs.dawn[,1])
+		All.probs.dawn<-as.matrix(All.probs.dawn[,-1])
+		}
+	}
+	
+	Diff<-try(Points.Land[which.max(apply(Phys.Mat[,1:(dim(Phys.Mat)[2])],1,  FUN=prod)),2]-prepared.data$Lat)
+	#if (class(Diff)=="try-error") Diff=NA
+
+	#Diff_1<-try(all.out$Points.Land[which.max(apply(all.out$Phys.Mat[,1:80],1,  FUN=prod)),2]-start[2])
+	#if (class(Diff_1)=="try-error") Diff_1=NA
+
+	return(Diff)
+}
+
+
+get.all.diffs<-function(Tracks, calibration, min.max.values=c(1, 1150), log.light.borders=log(c(2, 1100)), log.irrad.borders=c(-15, 50), cluster=NULL) {
+
+if (!is.null(cluster)) {
+	require(parallel)
+	#if (threads==-1) threads= detectCores()-1 # selecting all -1 available cores...
+	#mycl<-makeCluster(threads)
+	#tmp<-parallel:::clusterSetRNGStream(mycl)
+	### we don' need to send all parameters to node. so keep it easy..
+	tmp<-parallel:::clusterExport(cluster, c( "get.diff", "log.irrad.borders", "log.light.borders","calibration"),  envir=environment())
+	Diffs<-parSapply(cluster, Tracks, FUN=function(x) get.diff(prepared.data=x, calibration=calibration, min.max.values=min.max.values, log.light.borders=log.light.borders, log.irrad.borders=log.irrad.borders))
+	} else {
+	Diffs<-sapply(Tracks, FUN=function(x) get.diff(prepared.data=x, calibration=calibration, min.max.values=min.max.values, log.light.borders=log.light.borders, log.irrad.borders=log.irrad.borders))
+	}
+	return(Diffs)
+	}
+
+diffs.ll<-function(diffs, log=T) {
+# least squares
+ifelse(log, log(max(-1e70,mean(diffs^2, na.rm=T))), max(-1e70,mean(diffs^2, na.rm=T)))
+}
+
+test.deltas.old<-function(params, Tracks, Spline, calibration, min.max.values=c(1, 1150), log.light.borders=log(c(2, 1100)), log.irrad.borders=c(-15, 50), cluster=NULL) {
+# params are parameters for delta...
+print(params)
+deltas=params[1] + Spline%*%params[2:4] # for params - first for the intercept.
+Tracks <- mapply(append, Tracks, deltas, SIMPLIFY = FALSE)
+Tracks <- lapply(Tracks, FUN=function(x) {names(x)[length(x)]="delta"; return(x)})
+Diffs<-get.all.diffs(Tracks, calibration, min.max.values=min.max.values, log.light.borders=log.light.borders, log.irrad.borders=log.irrad.borders, cluster=cluster)
+plot(Diffs~sapply(Tracks, "[[", i=3))
+print(Diffs)
+Res<-diffs.ll(Diffs, log=F)
+cat("LL:", Res, "\n")
+return(Res)
+}
+
+
+test.deltas<-function(params, Tracks, Spline, calibration, min.max.values=c(1, 1150), log.light.borders=log(c(2, 1100)), log.irrad.borders=c(-15, 50), cluster=NULL) {
+# this function should send a spline as a calibration function..
+# params are parameters for delta...
+print(params)
+#deltas=params[1] + Spline%*%params[2:4] # for params - first for the intercept.
+
+#lat_correction_fun<-approxfun(y= bs(abs((-85:85)), knots=c(21, 42), degree=3, Boundary.knots=c(0,85), intercept=T)%*%params, x=-85:85)
+#lat_correction_fun<-approxfun(y= cbind(1, c(-85:85)^2,c (-85:85)^4)%*%params, x=-85:85)
+lat_correction_fun<-approxfun(y= cbind(1, cos(c(-85:85)/180*pi),cos(2*c(-85:85)/180*pi),cos(3*c(-85:85)/180*pi))%*%params, x=-85:85)
+
+# trying to add knots..
+calibration$lat_correction_fun=lat_correction_fun
+
+Diffs<-get.all.diffs(Tracks, calibration, min.max.values=min.max.values, log.light.borders=log.light.borders, log.irrad.borders=log.irrad.borders, cluster=cluster)
+par(mfrow=c(1,2))
+plot(lat_correction_fun(-85:85)~ c(-85:85))
+
+plot(Diffs~sapply(Tracks, "[[", i=3))
+Res<-diffs.ll(Diffs, log=F)
+cat("LL:", Res, "\n")
+return(Res)
+}
+
+
+
+
+
