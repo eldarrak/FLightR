32c1c049d3cc6aa06de262e04c3b598cf110bd84
Eldar Rakhimberdiev <eldarrak@gmail.com>
2015-03-18 22:37:09 +0100

    new corrections for nargs in lat correction

--- R/functions.Dusk.and.Dawn.5.2.R
+++ R/functions.Dusk.and.Dawn.5.2.R
@@ -354,8 +354,8 @@ get.current.slope.prob<-function(x, calibration=NULL, Twilight.solar.vector=NULL
 		Expected.mean<-time_correction
 		
 		if (is.null(delta)) {
-		if (is.null(calibration$lat_correction_fun)) {
-		delta=0 } else {
+		if (length(args(calibration$lat_correction_fun)==1)) {
+		delta=calibration$lat_correction_fun(x[2])} else {
 		delta=calibration$lat_correction_fun(x[2], Twilight.time.vector[1], calibration$Gamlatcalib) # have to check whether we always have time specified...
 		}
 		}
