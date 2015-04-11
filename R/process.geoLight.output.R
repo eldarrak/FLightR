127c0784164df1695335fdea778fa87c17e68ee7
Eldar laptop <eldarrak@gmail.com>
2015-04-11 17:41:35 +0200

    typos

--- R/process.geoLight.output.R
+++ R/process.geoLight.output.R
@@ -1,6 +1,7 @@
 #######
 # here I'll write a function that will take GeoLight output..
 
+process.geolight.output<-function(datetime, light, gl_twl) {
 
 #filter <- loessFilter(gl_twl[,1],gl_twl[,2],gl_twl[,3],k=10)
 Filtered_tw <- data.frame(datetime=as.POSIXct(c(gl_twl$tFirst,gl_twl$tSecond),"UTC"),type=c(gl_twl$type,ifelse(gl_twl$type==1,2,1)))
@@ -18,4 +19,8 @@ Data<-rbind(Data, data.frame(id=Filtered_tw$id, gmt= Filtered_tw$datetime, light
 All.p<-Data[order(Data$gmt),]
 #All.p<-All.p[!duplicated(All.p[,2:3], fromLast=T),]
 rownames(All.p)<-1:nrow(All.p)
+Filtered_tw$excluded=0
+FlightR.data=list(Data=All.p, twilights=Filtered_tw)
+return(FlightR.data)
+
 }
