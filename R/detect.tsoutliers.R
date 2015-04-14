d4fa3d2ff35708bb343a82ad851d08c8860bde86
Eldar Rakhimberdiev <eldarrak@gmail.com>
2015-04-14 11:35:45 +0200

    many more corrections for outliers

--- R/detect.tsoutliers.R
+++ R/detect.tsoutliers.R
@@ -1,7 +1,7 @@
 ############
 # detect ts outliers
 
-get.equatorial.max<-function(Proc.data, calibration, dusk=T, Twilight.ID) {
+get.equatorial.max<-function(Proc.data, calibration, dusk=T, Twilight.ID, center=NULL) {
 if (dusk) {
 Twilight.solar.vector<-solar(as.POSIXct(Proc.data$Twilight.time.mat.dusk[c(1:24, 26:49), Twilight.ID], tz="gmt", origin="1970-01-01"))
 Twilight.log.light.vector<-Proc.data$Twilight.log.light.mat.dusk[c(1:24, 26:49), Twilight.ID]
@@ -14,13 +14,17 @@ Twilight.time.vector=Proc.data$Twilight.time.mat.dawn[c(1:24, 26:49), Twilight.I
 
 #ok let's now create a line at equator
 Points.Land<-cbind(seq(-180, 180, length.out=360*2+1), 0, 1)
+ if (is.null(center)) {
+	Current.probs1<-	apply(Points.Land, 1, get.current.slope.prob, calibration=calibration,  Twilight.time.vector=Twilight.time.vector, Twilight.log.light.vector=Twilight.log.light.vector, plot=F, verbose=F,  log.light.borders=calibration$Parameters$log.light.borders, log.irrad.borders=calibration$Parameters$log.irrad.borders, dusk=dusk, Calib.param=calibration$Parameters$LogSlope, Twilight.solar.vector=Twilight.solar.vector, delta=NULL)
 
-Current.probs1<-	apply(Points.Land, 1, get.current.slope.prob, calibration=calibration,  Twilight.time.vector=Twilight.time.vector, Twilight.log.light.vector=Twilight.log.light.vector, plot=F, verbose=F,  log.light.borders=calibration$Parameters$log.light.borders, log.irrad.borders=calibration$Parameters$log.irrad.borders, dusk=dusk, Calib.param=calibration$Parameters$LogSlope, Twilight.solar.vector=Twilight.solar.vector, delta=NULL)
-
-Points.Land<-cbind(seq(Points.Land[which.max(Current.probs1),1]
--5, Points.Land[which.max(Current.probs1),1]
-+5, length.out=100), 0, 1)
-
+	Points.Land<-cbind(seq(Points.Land[which.max(Current.probs1),1]
+	-5, Points.Land[which.max(Current.probs1),1]
+	+5, length.out=100), 0, 1)
+	Points.Land[,1]<-Points.Land[,1]%%360
+	} else {
+	Points.Land<-cbind(seq(center-45, center+45, by=0.1), 0, 1)
+	Points.Land[,1]<-Points.Land[,1]%%360
+	}
 Current.probs1<-	apply(Points.Land, 1, get.current.slope.prob, calibration=calibration,  Twilight.time.vector=Twilight.time.vector, Twilight.log.light.vector=Twilight.log.light.vector, plot=F, verbose=F,  log.light.borders=calibration$Parameters$log.light.borders, log.irrad.borders=calibration$Parameters$log.irrad.borders, dusk=dusk, Calib.param=calibration$Parameters$LogSlope, Twilight.solar.vector=Twilight.solar.vector, delta=NULL)
 
 Final.max=Points.Land[which.max(Current.probs1),1]
@@ -46,7 +50,7 @@ mo2 <- locate.outliers.oloop(.Lons.ts, fit, types = otypes, maxit=15)
 
 
  if (nrow(mo2$outliers)<length(Lons)*0.075) {
- cat("adjusting cval\n")
+ cat("adjusting cval down\n")
  Cval=3.5
  while(nrow(mo2$outliers)<length(Lons)*0.075) {
  Cval=Cval*0.95
@@ -56,7 +60,7 @@ mo2 <- locate.outliers.oloop(.Lons.ts, fit, types = otypes, maxit=15)
  }
  
  if (nrow(mo2$outliers)>length(Lons)*0.1) {
- cat("adjusting cval\n")
+ cat("adjusting cval up\n")
  Cval=3.5
  while(nrow(mo2$outliers)>length(Lons)*0.1) {
  Cval=Cval*1.05
@@ -137,12 +141,79 @@ return(Res)
 }
 
 Deltas<--180:180
-Delta_cur<-Deltas[which.min(sapply(Deltas, ll.cut.point, Lons=Lons.dusk))]
-Lons.dusk<-((Lons.dusk+Delta_cur)%%360)-Delta_cur
+Delta_cur_dusk<-Deltas[which.min(sapply(Deltas, ll.cut.point, Lons=Lons.dusk))]
+Lons.dusk<-((Lons.dusk+Delta_cur_dusk)%%360)-Delta_cur_dusk
 
-Delta_cur<-Deltas[which.min(sapply(Deltas, ll.cut.point, Lons=Lons.dawn))]
-Lons.dawn<-Lons.dawn+((Lons.dusk+Delta_cur)%%360)-Delta_cur
+Delta_cur_dawn<-Deltas[which.min(sapply(Deltas, ll.cut.point, Lons=Lons.dawn))]
+Lons.dawn<-Lons.dawn+((Lons.dawn+Delta_cur_dawn)%%360)-Delta_cur_dawn
 
+#-------------------------------------------------
+# now we need to speciall focus on the outliers - maybe they were just estimated at the wrong maximum..
+loess.filter<-function(x, y, k=3) {
+Loess<-loess(y~x)
+Resid<-residuals(Loess)
+outliers<-which(abs(Resid)>(sd(Resid)*k))
+Res=list(outliers=outliers, center=predict(Loess)[outliers])
+return(Res)
+}
+
+Problematic.Dusks<-loess.filter(x=Proc.data$Twilight.time.mat.dusk[25,], y=Lons.dusk, k=3)
+Problematic.Dawns<-loess.filter(x=Proc.data$Twilight.time.mat.dawn[25,], y=Lons.dawn, k=3)
+
+par(mfrow=c(1,2))
+plot(x=Proc.data$Twilight.time.mat.dusk[25,], y=Lons.dusk, pch="+")
+if (length(Problematic.Dusks$outliers) >0) abline(v=Proc.data$Twilight.time.mat.dusk[25,Problematic.Dusks$outliers])
+plot(x=Proc.data$Twilight.time.mat.dawn[25,], y=Lons.dawn, pch="+")
+if (length(Problematic.Dawns$outliers) >0) abline(v=Proc.data$Twilight.time.mat.dawn[25,Problematic.Dawns$outliers])
+
+# for these points I'd like to rerun the estimation..
+if (length(Problematic.Dusks$outliers) >0| length(Problematic.Dawns$outliers)>0) {
+if (!is.null(Threads)) {
+	cat("making cluster\n")
+	require(parallel)
+	mycl <- parallel:::makeCluster(Threads)
+	tmp<-parallel:::clusterSetRNGStream(mycl)
+    tmp<-parallel:::clusterExport(mycl,c("calibration", "Proc.data"), envir=environment())
+    #tmp<-parallel:::clusterEvalQ(mycl, library("circular")) 
+    #tmp<-parallel:::clusterEvalQ(mycl, library("truncnorm")) 
+    tmp<-parallel:::clusterEvalQ(mycl, library("GeoLight")) 
+    tmp<-parallel:::clusterEvalQ(mycl, library("FLightR")) 
+	
+	if (length(Problematic.Dusks$outliers) >0) {
+	cat("reestimating dusk errors projection on equator\n")
+	Dusks<-cbind(Problematic.Dusks$outliers, Problematic.Dusks$center)
+	Lons.dusk_short<-parSapply(mycl, Dusks, FUN=function(x) get.equatorial.max(Proc.data, calibration, dusk=T, x[1], center=x[2]))
+	Lons.dusk_short<-((Lons.dusk_short+Delta_cur_dusk)%%360)-Delta_cur_dusk
+	Lons.dusk[Problematic.Dusks$outliers]<-Lons.dusk_short
+
+	}
+	
+	if (length(Problematic.Dawns$outliers) >0) {
+	cat("reestimating dawn errors projection on equator\n")
+	Dawns<-cbind(Problematic.Dawns$outliers, Problematic.Dawns$center)
+	Lons.dawn_short<-parSapply(mycl, Dawns, FUN=function(x) get.equatorial.max(Proc.data, calibration, dusk=F, x[1], center=x[2]))
+	Lons.dawn_short<-((Lons.dawn_short+Delta_cur_dawn)%%360)-Delta_cur_dawn
+	Lons.dawn[Problematic.Dawns$outliers]<-Lons.dawn_short
+
+	}
+	stopCluster(mycl)
+} else {
+	if (length(Problematic.Dusks$outliers) >0) {
+	cat("reestimating dusk errors projection on equator\n")
+	Dusks<-cbind(Problematic.Dusks$outliers, Problematic.Dusks$center)
+	Lons.dusk_short<-sapply(Dusks, FUN=function(x) get.equatorial.max(Proc.data, calibration, dusk=T, x[1], center=x[2]))
+	Lons.dusk_short<-((Lons.dusk_short+Delta_cur_dusk)%%360)-Delta_cur_dusk
+	Lons.dusk[Problematic.Dusks$outliers]<-Lons.dusk_short
+
+	}
+	
+	if (length(Problematic.Dawns$outliers) >0) {
+	cat("reestimating dawn errors projection on equator\n")
+	Dawns<-cbind(Problematic.Dawns$outliers, Problematic.Dawns$center)
+	Lons.dawn_short<-sapply(Dawns, FUN=function(x) get.equatorial.max(Proc.data, calibration, dusk=F, x[1], center=x[2]))
+	Lons.dawn_short<-((Lons.dawn_short+Delta_cur_dawn)%%360)-Delta_cur_dawn
+	}
+}
 cat("detecting outliers\n")
 
 #par(mfrow=c(2,1))
