fcef617b3aaef5042e11a8e479cbeff5b74190e2
Eldar Rakhimberdiev <eldarrak@gmail.com>
2015-04-24 13:28:37 +0200

    square into shape correction

--- R/functions.Dusk.and.Dawn.5.2.R
+++ R/functions.Dusk.and.Dawn.5.2.R
@@ -376,7 +376,7 @@ get.current.slope.prob<-function(x, calibration=NULL, Twilight.solar.vector=NULL
 		#sum<-mean(dlnorm(test.Slope, Expected.mean+delta, Calib.param[2]))
 		#-----------------
 		# new exp correction added 24 Apr 2015
-		sum<-mean(dlnorm(test.Slope, Expected.mean+delta, Calib.param[2])*test.Slope)
+		sum<-mean(dlnorm(test.Slope, Expected.mean+delta, Calib.param[2])*test.Slope^2)
 		#cat("time corr", time_correction ,"Expected.mean+delta", Expected.mean+delta, "\n")
 		#-----------------------
 				# correct for cos Lat
