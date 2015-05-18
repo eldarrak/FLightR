dc5b0ff651597513f782b299a91bdbbddb8da716
Eldar laptop <eldarrak@gmail.com>
2015-05-18 17:09:53 +0200

    zzz

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+print.FLightR.version <- function()
+{ library(help=FLightR)$info[[1]] -> version
+	version <- version[pmatch("Version",version)]
+	if(!is.null(version))
+	{
+		um <- strsplit(version," ")[[1]]
+  	    version <- um[nchar(um)>0][2]
+	}
+	hello <- paste("This is FLightR ",version,"\n",sep="")
+	packageStartupMessage(hello)
+}
+
+.onAttach <- function(...) { 
+	print.RMark.version()
+}
