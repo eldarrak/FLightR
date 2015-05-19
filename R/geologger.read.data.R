d6905d35d375572f6333e295148d55200150ebae
Eldar laptop <eldarrak@gmail.com>
2015-05-19 11:21:15 +0200

    Revert "Revert "data import functions combined""
    This reverts commit 1627f8e1a8cd04b92a6c1530c7a6f136f99fe24e.

--- R/geologger.read.data.R
+++ R/geologger.read.data.R
@@ -1,12 +1,1 @@
 
-
-geologger.read.data<-function( file) {
-	track <- read.csv(file,header=FALSE, stringsAsFactors=F) 
-	names(track)<-c('date','time','light')
-	track$datetime <- paste(track$date,track$time,sep=' ') #makes a new column called datetime with date and time concatenated together with a space between
-	track$gmt <- as.POSIXct(strptime(track$datetime,'%d/%m/%Y %H:%M:%S'),'UTC') #makes a new column called gmt with date and time data in a formate that R can work with
-	d <- data.frame(id =1:nrow(track), gmt = track$gmt, light = track$light) 
-	Data<-list(d=d)
-	return(Data)
-}
-
