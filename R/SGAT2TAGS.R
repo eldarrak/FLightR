3a7f2dd478fb230c2308587327284b0c5ee2ba9c
Eldar laptop <eldarrak@gmail.com>
2015-07-28 22:07:09 +0200

    sgattags mistake

--- R/SGAT2TAGS.R
+++ R/SGAT2TAGS.R
@@ -7,8 +7,9 @@ SGAT2TAGS <- function(raw, twl, threshold) {
   out <- data.frame(datetime = tmp01[,1], light = tmp01[,2],
                     twilight = ifelse(tmp01$Rise==TRUE & !is.na(tmp01$Rise), 1,
                                       ifelse(tmp01$Rise==FALSE & !is.na(tmp01$Rise), 2, 0)),
-                    interp = FALSE, exluded = ifelse(tmp01$Deleted & !is.na(tmp01$Deleted), TRUE, FALSE))
+                    interp = FALSE, excluded = ifelse(tmp01$Deleted & !is.na(tmp01$Deleted), TRUE, FALSE))
   out$interp[out$twilight>0] <- TRUE
   out<-out[order(out[,1]),]
   out[,1]<-format(out[,1], format="%Y-%m-%dT%H:%M:%S.000Z")
+  return(out)
  }
