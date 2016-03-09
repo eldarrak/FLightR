3edc7be4489cf57f80e07de8832517e4240a81ec
Eldar laptop <eldarrak@gmail.com>
2016-03-09 22:41:25 +0100

    correction for spatial mask.. still slow..

--- R/spatial.mask.R
+++ R/spatial.mask.R
@@ -2,8 +2,8 @@
 # the main feture is that it will search for any land in the ditance of xx meters, 
 
 
-create.land.mask<-function(Points, distance=25000) {
-
+create.land.mask<-function(Points, distance_km=25) {
+distance<-distance_km*1000
 Sp.All.Points.Focus<-SpatialPoints(Points, proj4string=CRS("+proj=longlat +datum=WGS84"))
 #############
 # ok, let's check 
@@ -15,20 +15,22 @@ Potential_water<-is.na(over( spTransform(Sp.All.Points.Focus, CRS("+proj=longlat
 
 wrld_simpl_t<-spTransform(wrld_simpl,  CRS=CRS(proj4string(Sp.All.Points.Focus)))
 
-Close_to_coast<-rep(0, length(Potential_water))
+#-----------------
+#Close_to_coast<-rep(0, length(Potential_water))
 
-for (i in 1:nrow(All.Points.Focus)) {
-if (Potential_water[i]==0) next 
-Close_to_coast[i]<-as.numeric(gWithinDistance(Sp.All.Points.Focus[i,], wrld_simpl_t, 1))
-cat("\r",i)
-}
-
+#for (i in 1:nrow(Points)) {
+#if (Potential_water[i]==0) next 
+#Close_to_coast[i]<-as.numeric(gWithinDistance(Sp.All.Points.Focus[i,], wrld_simpl_t, ))
+#cat("\r",i)
+#}
+#--------------------
 # and now, for these special poitns we want to double check in the right projection
 
 Land<-as.numeric(!Potential_water)
 
 for (i in 1:nrow(Points)) {
-if (Close_to_coast[i]==1) {
+#if (Close_to_coast[i]==1) {
+if (Potential_water[i]==TRUE) {
 Land[i]<-as.numeric(gWithinDistance( spTransform(Sp.All.Points.Focus[i,], CRS(paste("+proj=aeqd +lon=", Sp.All.Points.Focus[i,]@coords[1], "lat=", Sp.All.Points.Focus[i,]@coords[2], sep=""))), spTransform(wrld_simpl, CRS(paste("+proj=aeqd +lon=", Sp.All.Points.Focus[i,]@coords[1], "lat=", Sp.All.Points.Focus[i,]@coords[2], sep=""))), distance))
 cat("\r",i)
 }
