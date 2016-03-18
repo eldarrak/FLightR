9349e442518d1a13b8dc6828cd1876fef26a0f85
Eldar laptop <eldarrak@gmail.com>
2016-03-18 14:47:37 +0100

    numeric to real

--- R/run_particle_filter.R
+++ R/run_particle_filter.R
@@ -147,7 +147,7 @@ generate.points.dirs<-function(x , in.Data, Current.Proposal, a=45, b=500) {
 	#if (in.Data$Spatial$Grid[x[[1]],3]==0) x[[3]]=x[[2]]
 	# end of addition
     Dists.distr<-in.Data$Spatial$tmp$Distance[x[[1]],]	
-    Dists.probs<-dtruncnorm(Dists.distr, a=a, b=b, Current.Proposal$M.mean, Current.Proposal$M.sd)
+    Dists.probs<-dtruncnorm(as.numeric(Dists.distr), a=a, b=b, Current.Proposal$M.mean, Current.Proposal$M.sd)
     ###
     #  library(fields)
     # dists
@@ -763,7 +763,7 @@ mu.sigma.truncnorm<-function(x, a=45, b=500) {
 # this is used optionally
   if (length(unique(x))>1) {
     tr.norm<-function(prm) {
-      sum(-log(truncnorm:::dtruncnorm(x,a=a,b=b,mean=prm[1],sd=prm[2])))
+      sum(-log(truncnorm:::dtruncnorm(as.numeric(x),a=a,b=b,mean=prm[1],sd=prm[2])))
     }
     Res=try(optim(c(mean(x),sd(x)), tr.norm, method="BFGS"))
     #if (class(Res)=="try-error") Res=try(optim(c(mean(x),sd(x)), tr.norm,method="SANN"))
