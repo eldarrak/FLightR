106fa577d7feae8571b66a01c487a0561c35e13e
Eldar laptop <eldarrak@gmail.com>
2016-10-11 15:20:47 +0200

    format correction

--- R/get.prob.of.being.in.R
+++ R/get.prob.of.being.in.R
@@ -52,12 +52,11 @@ Q4<-find.time(Prob.of.being.in, time, quantiles[4], plot=F)
 Q5<-find.time(Prob.of.being.in, time, quantiles[5], plot=F)
 
 # find pairs for each in Q3
-Res<-data.frame(Q.025=Q1[sapply(Q3, FUN=function(x) which.min(abs(x - Q1)))], 
-		Q.25=Q2[sapply(Q3, FUN=function(x) which.min(abs(x - Q2)))],
-		Q.50=Q3,
-		Q.75=ifelse(!is.na(Q4), Q4[sapply(Q3, FUN=function(x) which.min(abs(x - Q4)))],NA),  
-		Q.975=ifelse(!is.na(Q5) ,Q5[sapply(Q3, FUN=function(x) which.min(abs(x - Q5)))], NA))
-
+Res<-data.frame(Q.025=as.POSIXct(Q1[sapply(Q3, FUN=function(x) which.min(abs(x - Q1)))], origin='1970-01-01'), 
+		Q.25=as.POSIXct(Q2[sapply(Q3, FUN=function(x) which.min(abs(x - Q2)))], origin='1970-01-01'),
+		Q.50=as.POSIXct(Q3,origin='1970-01-01'),
+		Q.75=as.POSIXct(ifelse(!is.na(Q4), Q4[sapply(Q3, FUN=function(x) which.min(abs(x - Q4)))],NA), origin='1970-01-01'),  
+		Q.975=as.POSIXct(ifelse(!is.na(Q5) ,Q5[sapply(Q3, FUN=function(x) which.min(abs(x - Q5)))], NA),origin='1970-01-01'))
 	return(Res)
 	}
 
