0d14b2f7cde4c4d3dcb93dc1135ba5d2ce11abe6
Eldar laptop <eldarrak@gmail.com>
2016-12-11 21:13:39 +0100

    another change to test to pass linix test

--- tests/testthat/test_data_processing.R
+++ tests/testthat/test_data_processing.R
@@ -1,12 +1,9 @@
 test_that('plot_slopes_by_location works',  {
    File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
    Proc.data<-get.tags.data(File)
-   pdf('plot_slopes_by_location.pdf')
-   plot_slopes_by_location(Proc.data=Proc.data, location=c(5.43, 52.93))
-   abline(v=as.POSIXct("2013-08-20")) # end of first calibration period
-   abline(v=as.POSIXct("2014-05-05")) # start of the second calibration period
-   dev.off()
-   expect_equal(34, round(file.size('plot_slopes_by_location.pdf')/100))
+   expect_silent(plot_slopes_by_location(Proc.data=Proc.data, location=c(5.43, 52.93)))
+   expect_silent(abline(v=as.POSIXct("2013-08-20"))) # end of first calibration period
+   expect_silent(abline(v=as.POSIXct("2014-05-05"))) # start of the second calibration period
    }
 )
 
