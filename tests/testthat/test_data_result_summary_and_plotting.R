7cc606a3c1c647de312f0ba7f9b52a4aceee8a96
Eldar laptop <eldarrak@gmail.com>
2016-12-13 11:33:21 +0100

    test for summary function

--- tests/testthat/test_data_result_summary_and_plotting.R
+++ tests/testthat/test_data_result_summary_and_plotting.R
@@ -57,3 +57,23 @@ test_that('plot_util_distr_works',  {
    expect_warning(plot_util_distr(Result, zoom=5))
    }
 )
+
+
+test_that('summary produces null foor stationary tag',  {
+   File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
+   Proc.data<-get.tags.data(File, end.date=as.POSIXct('2013-08-21', tz='GMT'))
+   Calibration.periods<-data.frame(
+        calibration.start=NA,
+        calibration.stop=as.POSIXct("2013-08-20"),
+		lon=5.43, lat=52.93) 
+   Calibration<-make.calibration(Proc.data, Calibration.periods, likelihood.correction=FALSE)
+   Grid<-make.grid(left=0, bottom=50, right=10, top=56,
+     distance.from.land.allowed.to.use=c(-Inf, Inf),
+     distance.from.land.allowed.to.stay=c(-Inf, Inf))
+   all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
+   Result<-run.particle.filter(all.in, threads=1,
+           nParticles=1e4, known.last=TRUE, check.outliers=FALSE)
+   # not too clever but it looks like that other ways of graphics test are not stable..
+   expect_null(stationary.migration.summary(Result))
+   }
+)
