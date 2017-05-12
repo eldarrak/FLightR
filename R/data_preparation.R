a2b291dbfc02f5de8428883cbed0df7087a4f3f0
Eldar laptop <eldarrak@gmail.com>
2017-05-12 11:18:33 +0200

    more in stability

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -206,8 +206,12 @@ find.stationary.location<-function(Proc.data, calibration.start,  calibration.st
      	   if (print.optimization) cat(paste(initial.coords[1], initial.coords[2], calibration.parameters$All.slopes$Parameters$LogSlope[1], calibration.parameters$All.slopes$Parameters$LogSlope[2], percent_excluded), '\n')
           return(calibration.parameters$All.slopes$Parameters$LogSlope[2]^2+percent_excluded)
 	   } else {
+	       if (length(unique(calibration.parameters$All.slopes$Slopes$Type))==1) {
+		   Val<-10
+		   } else {
      	   Val<-log(1/(stats::anova(stats::lm(logSlope~Time+I(Time^2)+Type, data=calibration.parameters$All.slopes$Slopes),stats::lm(logSlope~Time, data=calibration.parameters$All.slopes$Slopes))[2,6]))+percent_excluded
-     	   if (print.optimization) cat(paste(initial.coords[1], initial.coords[2], calibration.parameters$All.slopes$Parameters$LogSlope[1], calibration.parameters$All.slopes$Parameters$LogSlope[2]), Val, '\n')
+		   }
+     	   if (print.optimization) cat(paste(initial.coords[1], initial.coords[2], calibration.parameters$All.slopes$Parameters$LogSlope[1], calibration.parameters$All.slopes$Parameters$LogSlope[2]), Val,  percent_excluded, '\n')
            return(Val)
 	   }
    }
