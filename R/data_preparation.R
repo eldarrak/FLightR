d4fe1bc91e31b7350a26581bd33827f77c2898ee
Eldar laptop <eldarrak@gmail.com>
2017-05-12 11:45:44 +0200

    stronger boundaries on exclusion

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -206,12 +206,11 @@ find.stationary.location<-function(Proc.data, calibration.start,  calibration.st
      	   if (print.optimization) cat(paste(initial.coords[1], initial.coords[2], calibration.parameters$All.slopes$Parameters$LogSlope[1], calibration.parameters$All.slopes$Parameters$LogSlope[2], percent_excluded), '\n')
           return(calibration.parameters$All.slopes$Parameters$LogSlope[2]^2+percent_excluded)
 	   } else {
-	       print(table(calibration.parameters$All.slopes$Slopes$Type))
-	       if (min(table(calibration.parameters$All.slopes$Slopes$Type))<=3) {
+	       if (min(table(calibration.parameters$All.slopes$Slopes$Type))<=2) {
 		   print('only_one_twilight_type_left!\n')
-		   Val<-10
+		   Val<-10+percent_excluded
 		   } else {
-     	   Val<-log(1/(stats::anova(stats::lm(logSlope~Time+I(Time^2)+Type, data=calibration.parameters$All.slopes$Slopes),stats::lm(logSlope~Time, data=calibration.parameters$All.slopes$Slopes))[2,6]))+percent_excluded
+     	   Val<-log(1/(stats::anova(stats::lm(logSlope~Time+I(Time^2)+Type, data=calibration.parameters$All.slopes$Slopes),stats::lm(logSlope~Time, data=calibration.parameters$All.slopes$Slopes))[2,6]))+percent_excluded^2
 		   }
      	   if (print.optimization) cat(paste(initial.coords[1], initial.coords[2], calibration.parameters$All.slopes$Parameters$LogSlope[1], calibration.parameters$All.slopes$Parameters$LogSlope[2]), Val,  percent_excluded, '\n')
            return(Val)
