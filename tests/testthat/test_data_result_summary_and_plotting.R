c1896f19627a8048f13af7910a48c36f1e0673fa
Eldar laptop <eldarrak@gmail.com>
2018-10-08 16:34:39 +0200

    excluded tests ggmap

--- tests/testthat/test_data_result_summary_and_plotting.R
+++ tests/testthat/test_data_result_summary_and_plotting.R
@@ -18,45 +18,45 @@ test_that('plot_lon_lat_works',  {
    }
 )
 
-test_that('map_flightr_ggmap_works',  {
-   File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
-   Proc.data<-get.tags.data(File, end.date=as.POSIXct('2013-07-02', tz='GMT'))
-   Calibration.periods<-data.frame(
-        calibration.start=NA,
-        calibration.stop=as.POSIXct("2013-08-20"),
-		lon=5.43, lat=52.93) 
-   Calibration<-make.calibration(Proc.data, Calibration.periods, likelihood.correction=FALSE)
-   Grid<-make.grid(left=0, bottom=50, right=10, top=56,
-     distance.from.land.allowed.to.use=c(-Inf, Inf),
-     distance.from.land.allowed.to.stay=c(-Inf, Inf))
-   all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
-   Result<-run.particle.filter(all.in, threads=1,
-           nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
-   # not too clever but it looks like that other ways of graphics test are not stable..
-   p<-map.FLightR.ggmap(Result, seasonal.donut.location=NULL,return.ggobj=TRUE, zoom=5, save=FALSE)    
-   expect_equal(length(p), 9)
-   }
-)
+# test_that('map_flightr_ggmap_works',  {
+   # File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
+   # Proc.data<-get.tags.data(File, end.date=as.POSIXct('2013-07-02', tz='GMT'))
+   # Calibration.periods<-data.frame(
+        # calibration.start=NA,
+        # calibration.stop=as.POSIXct("2013-08-20"),
+		# lon=5.43, lat=52.93) 
+   # Calibration<-make.calibration(Proc.data, Calibration.periods, likelihood.correction=FALSE)
+   # Grid<-make.grid(left=0, bottom=50, right=10, top=56,
+     # distance.from.land.allowed.to.use=c(-Inf, Inf),
+     # distance.from.land.allowed.to.stay=c(-Inf, Inf))
+   # all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
+   # Result<-run.particle.filter(all.in, threads=1,
+           # nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
+   # # not too clever but it looks like that other ways of graphics test are not stable..
+   # p<-map.FLightR.ggmap(Result, seasonal.donut.location=NULL,return.ggobj=TRUE, zoom=5, save=FALSE)    
+   # expect_equal(length(p), 9)
+   # }
+# )
 
 
-test_that('plot_util_distr_works',  {
-   File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
-   Proc.data<-get.tags.data(File, end.date=as.POSIXct('2013-07-02', tz='GMT'))
-   Calibration.periods<-data.frame(
-        calibration.start=NA,
-        calibration.stop=as.POSIXct("2013-08-20"),
-		lon=5.43, lat=52.93) 
-   Calibration<-make.calibration(Proc.data, Calibration.periods, likelihood.correction=FALSE)
-   Grid<-make.grid(left=0, bottom=50, right=10, top=56,
-     distance.from.land.allowed.to.use=c(-Inf, Inf),
-     distance.from.land.allowed.to.stay=c(-Inf, Inf))
-   all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
-   Result<-run.particle.filter(all.in, threads=1,
-           nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
-   # not too clever but it looks like that other ways of graphics test are not stable..
-   expect_warning(plot_util_distr(Result, zoom=5, save=FALSE))
-   }
-)
+# test_that('plot_util_distr_works',  {
+   # File<-system.file("extdata", "Godwit_TAGS_format.csv", package = "FLightR")
+   # Proc.data<-get.tags.data(File, end.date=as.POSIXct('2013-07-02', tz='GMT'))
+   # Calibration.periods<-data.frame(
+        # calibration.start=NA,
+        # calibration.stop=as.POSIXct("2013-08-20"),
+		# lon=5.43, lat=52.93) 
+   # Calibration<-make.calibration(Proc.data, Calibration.periods, likelihood.correction=FALSE)
+   # Grid<-make.grid(left=0, bottom=50, right=10, top=56,
+     # distance.from.land.allowed.to.use=c(-Inf, Inf),
+     # distance.from.land.allowed.to.stay=c(-Inf, Inf))
+   # all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
+   # Result<-run.particle.filter(all.in, threads=1,
+           # nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
+   # # not too clever but it looks like that other ways of graphics test are not stable..
+   # expect_warning(plot_util_distr(Result, zoom=5, save=FALSE))
+   # }
+# )
 
 
  test_that('summary produces null foor stationary tag',  {
