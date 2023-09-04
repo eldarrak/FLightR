fef55a70d806b47e17edbca66b29a0b260f36571
Eldar Rakhimberdiev <eldarrak@gmail.com>
2023-09-04 11:20:17 +0200

    0.5.4 update

--- tests/testthat/test_data_result_summary_and_plotting.R
+++ tests/testthat/test_data_result_summary_and_plotting.R
@@ -33,7 +33,7 @@ test_that('map_flightr_ggmap_works',  {
    all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
    Result<-run.particle.filter(all.in, threads=1,
            nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
-   if (utils::packageVersion('ggmap')[1]>=2.7) {
+   if (utils::packageVersion('ggmap')[1]>='2.7') {
    if (ggmap::has_google_key()) {
          # not too clever but it looks like that other ways of graphics test are not stable..
          p<-map.FLightR.ggmap(Result, seasonal.donut.location=NULL,return.ggobj=TRUE, zoom=5, save=FALSE, plot.cloud=FALSE)    
@@ -66,7 +66,7 @@ test_that('map_flightr_ggmap_works',  {
    all.in<-make.prerun.object(Proc.data, Grid, start=c(5.43, 52.93), Calibration=Calibration, threads=1)
    Result<-run.particle.filter(all.in, threads=1,
            nParticles=1e3, known.last=TRUE, check.outliers=FALSE)
-   if (utils::packageVersion('ggmap')[1]>=2.7) {
+   if (utils::packageVersion('ggmap')[1]>='2.7') {
    if (ggmap::has_google_key()) {
    # not too clever but it looks like that other ways of graphics test are not stable..
    expect_type(plot_util_distr(Result, zoom=5, save=FALSE), 'list')
